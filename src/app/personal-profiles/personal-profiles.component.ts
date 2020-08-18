51f5fbaa476b8af0270ea2dad45499ce258d1ab2
HuynhNhu <huynhnhudo10@gmail.com>
2020-08-18 17:55:11 +0700

    Menu

--- src/app/personal-profiles/personal-profiles.component.ts
+++ src/app/personal-profiles/personal-profiles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from 'src/shared/models/link';
 
 @Component({
   selector: 'app-personal-profiles',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-profiles.component.scss']
 })
 export class PersonalProfilesComponent implements OnInit {
-
+  links: Link[] = [
+    {name: 'About', url: '#'},
+    {name: 'Project', url: '#'},
+    {name: 'Contact', url: '#'}
+  ]
+  activeLinkByName: string = 'About';
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  navigateUrl(linkName: string) {
+    this.activeLinkByName = linkName;
+  }
 }
