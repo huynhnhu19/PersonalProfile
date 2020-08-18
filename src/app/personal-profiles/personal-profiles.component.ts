63b95de8d387b290cdd0de354bd0adbc2404f2d7
HuynhNhu <huynhnhudo10@gmail.com>
2020-08-18 15:46:58 +0700

    Init project

---
+++ src/app/personal-profiles/personal-profiles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-profiles',
+  templateUrl: './personal-profiles.component.html',
+  styleUrls: ['./personal-profiles.component.scss']
+})
+export class PersonalProfilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
